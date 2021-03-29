60dddcbf6435d17e1bda02d9fd3f2a52a6ffebdf
andreyshilenko99 <31961611+andreyshilenko99@users.noreply.github.com>
2021-03-29 16:06:38 +0300

    koe kak robit

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
 using System.Threading;
 using UGCS.Sdk.Protocol;
 using UGCS.Sdk.Protocol.Encoding;
 using UGCS.Sdk.Tasks;
-
+using TcpClient = UGCS.Sdk.Protocol.TcpClient;
+using TcpClientt = System.Net.Sockets;
 
 namespace DirectVehicleControl
 {
-   
-
     class Program
     {
         static void Main(string[] args)
@@ -45,101 +47,173 @@ namespace DirectVehicleControl
             // Id of the emu-copter is 2
             var vehicleToControl = new Vehicle {Id = 2};
 
-            Listener lol = new Listener();
-            lol.Connect();
-            
-            
-            
-            // takeoff
-            SendCommandRequest takeoff = new SendCommandRequest
+            TcpClientt.TcpListener server = new TcpClientt.TcpListener(IPAddress.Any, 777);
+            server.Start(); // запускаем сервер
+            byte[] ok = new byte[100]; 
+            ok = Encoding.Default.GetBytes("ok"); 
+            while (true) // бесконечный цикл обслуживания клиентов
             {
-                ClientId = clientId,
-                Command = new UGCS.Sdk.Protocol.Encoding.Command
+                TcpClientt.TcpClient client = server.AcceptTcpClient(); // ожидаем подключение клиента
+                TcpClientt.NetworkStream ns = client.GetStream(); // для получения и отправки сообщений
+                // byte[] ok = new byte[100]; 
+                // ok = Encoding.Default.GetBytes("ok"); TODO Connected
+                //
+                // ns.Write(ok, 0, ok.Length);
+                while (client.Connected) // пока клиент подключен, ждем приходящие сообщения
                 {
-                    Code = "takeoff_command",
-                    Subsystem = Subsystem.S_FLIGHT_CONTROLLER,
-                    Silent = false,
-                    ResultIndifferent = false
+                    byte[] msg = new byte[10]; // готовим место для принятия сообщения
+                    int count = ns.Read(msg, 0, msg.Length); // читаем сообщение от клиента
+                    Console.Write(Encoding.Default.GetString(msg, 0,
+                        count)); // выводим на экран полученное сообщение в виде строки
+                    string result = Encoding.Default.GetString(msg);
+                    
+                    switch (result)
+                    {
+                        case "takeoff___":
+                        {
+                            
+                            SendCommandRequest takeoff = new SendCommandRequest
+                            {
+                                ClientId = clientId,
+                                Command = new UGCS.Sdk.Protocol.Encoding.Command
+                                {
+                                    Code = "takeoff_command",
+                                    Subsystem = Subsystem.S_FLIGHT_CONTROLLER,
+                                    Silent = true,
+                                    ResultIndifferent = true
+                                }
+                            };
+                            takeoff.Vehicles.Add(vehicleToControl);
+                            var takeoffCmd = messageExecutor.Submit<SendCommandResponse>(takeoff);
+                            takeoffCmd.Wait();
+                            Thread.Sleep(5000);
+                            ns.Write(ok, 0, ok.Length);
+                            break;
+                        }
+                        case "dir_vehcon":
+                        {
+                            // Vehicle control in joystick mode
+                            SendCommandRequest vehicleJoystickControl = new SendCommandRequest
+                            {
+                                ClientId = clientId,
+                                Command = new UGCS.Sdk.Protocol.Encoding.Command
+                                {
+                                    Code = "direct_vehicle_control",
+                                    Subsystem = Subsystem.S_FLIGHT_CONTROLLER,
+                                    Silent = true,
+                                    ResultIndifferent = true
+                                }
+                            };
+                            ns.Write(ok, 0, ok.Length); // отправляем сообщени
+                            byte[] income = new byte[7]; // готовим место для принятия сообщения
+                            int counter = ns.Read(income, 0, income.Length); // читаем сообщение от клиента
+                            Console.Write(Encoding.Default.GetString(income, 0,
+                                counter)); // выводим на экран полученное сообщение в виде строки
+                            //List of current joystick values to send to vehicle.
+                            List<CommandArgument> listJoystickCommands = new List<CommandArgument>();
+                            var part = income.ToString().Split(",");
+                            switch (part[0])
+                            {
+                                case "roll":
+                                    listJoystickCommands.Add(new CommandArgument
+                                    {
+                                        Code = "roll",
+                                        Value = new Value() {DoubleValue = Convert.ToDouble(part[1])}
+                                    });
+                                    break;
+                                case "pitch":
+                                    listJoystickCommands.Add(new CommandArgument
+                                    {
+                                        Code = "pitch",
+                                        Value = new Value() {DoubleValue = Convert.ToDouble(part[1])}
+                                    });
+                                    break;
+                                case "throttle":
+                                    listJoystickCommands.Add(new CommandArgument
+                                    {
+                                        Code = "throttle",
+                                        Value = new Value() {DoubleValue = Convert.ToDouble(part[1])}
+                                    });
+                                    break;
+                                case "yaw":
+                                    listJoystickCommands.Add(new CommandArgument
+                                    {
+                                        Code = "yaw",
+                                        Value = new Value() {DoubleValue = Convert.ToDouble(part[1])}
+                                    });
+                                    break;
+                                case "exit":
+                                    break;
+                            }
+
+                            vehicleJoystickControl.Command.Arguments.AddRange(listJoystickCommands);
+                            var sendJoystickCommandResponse =
+                                messageExecutor.Submit<SendCommandResponse>(vehicleJoystickControl);
+                            sendJoystickCommandResponse.Wait();
+                            System.Console.WriteLine("Was sent {0}", part[0]);
+                            Thread.Sleep(2000);
+                            break;
+                        }
+                        case "direct_payload_control":
+                        {
+                            break;
+                        }
+                        case "landcom":
+                        {
+                            SendCommandRequest land = new SendCommandRequest
+                            {
+                                ClientId = clientId,
+                                Command = new UGCS.Sdk.Protocol.Encoding.Command
+                                {
+                                    Code = "land_command",
+                                    Subsystem = Subsystem.S_FLIGHT_CONTROLLER,
+                                    Silent = false,
+                                    ResultIndifferent = false
+                                }
+                            };
+                            land.Vehicles.Add(vehicleToControl);
+                            var landCmd = messageExecutor.Submit<SendCommandResponse>(land);
+                            landCmd.Wait();
+                            Thread.Sleep(5000);
+                            ns.Write(ok, 0, ok.Length);
+                            break;
+                        }
+                        case "joystik":
+                        {
+                            SendCommandRequest joystickModeCommand = new SendCommandRequest
+                            {
+                                ClientId = clientId,
+                                Command = new UGCS.Sdk.Protocol.Encoding.Command
+                                {
+                                    Code = "joystick",
+                                    Subsystem = Subsystem.S_FLIGHT_CONTROLLER,
+                                    Silent = false,
+                                    ResultIndifferent = false
+                                }
+                            };
+
+                            joystickModeCommand.Vehicles.Add(vehicleToControl);
+                            var joystickMode = messageExecutor.Submit<SendCommandResponse>(joystickModeCommand);
+                            joystickMode.Wait();
+                            ns.Write(ok, 0, ok.Length);
+                            break;
+                        }
+                        case "manual":
+                        {
+                            break;
+                        }
+                    }
+
+                    
                 }
-            };
-            takeoff.Vehicles.Add(vehicleToControl);
-            var takeoffCmd = messageExecutor.Submit<SendCommandResponse>(takeoff);
-            takeoffCmd.Wait();
-            Thread.Sleep(5000);
-
-
-            //Go to joystick mode
-            SendCommandRequest joystickModeCommand = new SendCommandRequest
-            {
-                ClientId = clientId,
-                Command = new UGCS.Sdk.Protocol.Encoding.Command
-                {
-                    Code = "joystick",
-                    Subsystem = Subsystem.S_FLIGHT_CONTROLLER,
-                    Silent = false,
-                    ResultIndifferent = false
-                }
-            };
-            joystickModeCommand.Vehicles.Add(vehicleToControl);
-            var joystickMode = messageExecutor.Submit<SendCommandResponse>(joystickModeCommand);
-            joystickMode.Wait();
-
-            // Vehicle control in joystick mode
-            SendCommandRequest vehicleJoystickControl = new SendCommandRequest
-            {
-                ClientId = clientId,
-                Command = new UGCS.Sdk.Protocol.Encoding.Command
-                {
-                    Code = "direct_vehicle_control",
-                    Subsystem = Subsystem.S_FLIGHT_CONTROLLER,
-                    Silent = true,
-                    ResultIndifferent = true
-                }
-            };
-            vehicleJoystickControl.Vehicles.Add(vehicleToControl);
-
-            //List of current joystick values to send to vehicle.
-            List<CommandArgument> listJoystickCommands = new List<CommandArgument>();
-            listJoystickCommands.Add(new CommandArgument
-            {
-                Code = "roll",
-                Value = new Value() {DoubleValue = 0}
-            });
-            listJoystickCommands.Add(new CommandArgument
-            {
-                Code = "pitch",
-                Value = new Value() {DoubleValue = 0}
-            });
-            listJoystickCommands.Add(new CommandArgument
-            {
-                Code = "yaw",
-                Value = new Value() {DoubleValue = 1}
-            });
-            listJoystickCommands.Add(new CommandArgument
-            {
-                Code = "throttle",
-                Value = new Value() {DoubleValue = 0}
-            });
-            
-
-            vehicleJoystickControl.Command.Arguments.AddRange(listJoystickCommands);
-
-            for (int i = 1; i < 11; i++)
-            {
-                var sendJoystickCommandResponse = messageExecutor.Submit<SendCommandResponse>(vehicleJoystickControl);
-                sendJoystickCommandResponse.Wait();
-                System.Console.WriteLine("Joystick command to go UP {0}", i);
-                Thread.Sleep(2000);
+                System.Console.ReadKey();
+                tcpClient.Close();
+                messageSender.Cancel();
+                messageReceiver.Cancel();
+                messageExecutor.Close();
+                notificationListener.Dispose();
             }
-
-
-            System.Console.ReadKey();
-
-            tcpClient.Close();
-            messageSender.Cancel();
-            messageReceiver.Cancel();
-            messageExecutor.Close();
-            notificationListener.Dispose();
+            
         }
     }
 }
